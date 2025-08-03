da62ea64338324eb5c1ec53f5ccc83c7b7c5e5e7
Pablo Berdasco <pberdasco@gmail.com>
2025-08-03 17:57:03 -0300

    feat: refactor Instrumento model to extend BaseModel and streamline JSON conversion

--- src/models/maestros/instrumentos_model.js
+++ src/models/maestros/instrumentos_model.js
@@ -1,38 +1,28 @@
-export default class Muestra {
-    id; // int
-    tipoInstrumentoId; // int (FK)
-    tipoInstrumento; // string(30) - join
-    claseInstrumento; // string(10) - join
-    emisorId; // int (FK)
-    emisor; // string (45) - join
-    ticker; // string(10)
-    notas; // string(150)
+// instrumentos_model.js
+import { BaseModel } from '../baseModel.js';
+
+export default class Instrumento extends BaseModel {
+    static fields = [
+        { name: 'id', type: 'int' },
+        { name: 'tipoInstrumentoId', type: 'int', fk: true },
+        { name: 'tipoInstrumento', type: 'string', join: true },
+        { name: 'claseInstrumento', type: 'string', join: true },
+        { name: 'emisorId', type: 'int', fk: true },
+        { name: 'emisor', type: 'string', join: true },
+        { name: 'ticker', type: 'string' },
+        { name: 'notas', type: 'string' }
+    ];
 
     constructor (row) {
-        this.id = row.id;
-        this.tipoInstrumentoId = row.tipoInstrumentoId;
-        this.tipoInstrumento = row.tipoInstrumento;
-        this.claseInstrumento = row.claseInstrumento;
-        this.emisorId = row.emisorId;
-        this.emisor = row.emisor;
-        this.ticker = row.ticker;
-        this.notas = row.notas;
+        super(row, Instrumento.fields);
+        // ? Parseo o logica especifica
     }
 
     toJson () {
-        return {
-            id: this.id,
-            tipoInstrumentoId: this.tipoInstrumentoId,
-            tipoInstrumento: this.tipoInstrumento,
-            claseInstrumento: this.claseInstrumento,
-            emisorId: this.emisorId,
-            emisor: this.emisor,
-            ticker: this.ticker,
-            notas: this.notas
-        };
+        return BaseModel.toJson(this, Instrumento.fields);
     }
 
     static fromRows (rows) {
-        return rows.map(row => new Muestra(row));
+        return BaseModel.fromRows(rows, Instrumento.fields, Instrumento);
     }
 }
